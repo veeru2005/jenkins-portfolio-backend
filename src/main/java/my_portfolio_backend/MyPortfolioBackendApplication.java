4a1b1d7b1a1ac646c62c28bb1cfbd5d26afb4168
veeru2005 <163710790+veeru2005@users.noreply.github.com>
2025-09-12 00:41:08 +0530

    Update MyPortfolioBackendApplication.java

--- src/main/java/my_portfolio_backend/MyPortfolioBackendApplication.java
+++ src/main/java/my_portfolio_backend/MyPortfolioBackendApplication.java
@@ -2,12 +2,18 @@ package my_portfolio_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyPortfolioBackendApplication {
+public class MyPortfolioBackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyPortfolioBackendApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyPortfolioBackendApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(MyPortfolioBackendApplication.class, args);
+    }
 }
